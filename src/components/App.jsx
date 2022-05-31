1cb07b6ef92be60ab85ea5bc6898cc94095fe58c
diemwell <welldiem@gmail.com>
2022-05-30 21:09:02 -0400

    created file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-    <div>
-        App
+import useStyles from './styles';
+
+import { Coaches, CoachInformation, NavBar, Profile } from './index.js'
+
+const App = () => {
+    const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar/>
+      <main className={classes.content}>
+      <div className={classes.toolbar}/>
+        <Switch>
+            <Route exact path="/">
+                <h1>Home</h1>
+            </Route>
+            <Route exact path="/coaches">
+                <Coaches/>
+            </Route>
+            <Route exact path="/coach/:username">
+                <CoachInformation/>
+            </Route>
+            <Route exact path="/profile/:username">
+                <Profile/>
+            </Route>
+        </Switch>
+        </main>
     </div>
-);
+  );
+}
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Coaches } from './Coaches/Coaches';
+export { default as CoachInformation } from './CoachInformation/CoachInformation';
+export { default as NavBar } from './NavBar/NavBar';
+export { default as Profile } from './Profile/Profile';
